beb71910161f01060420d801a655f4357a5c60a0
Vinay <vinayojha1976@gmail.com>
2025-06-14 14:06:54 +0530

    deployment error resolved

--- client/src/Pages/dashboarduser/dashboard/DashboardLanding.tsx
+++ client/src/Pages/dashboarduser/dashboard/DashboardLanding.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 // import { motion } from "motion/react";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -7,7 +6,6 @@ import {
   CardContent,
   CardHeader,
   CardTitle,
-  CardDescription,
 } from "@/components/ui/card";
 import { motion } from "framer-motion";
 
